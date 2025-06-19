588068fc0b3764b947926e49d9fbc7589f1032ef
Jan Šustr <jan.sustr11@gmail.com>
2025-06-19 17:07:21 +0200

    u

--- mocks/mock_command_runner.ts
+++ mocks/mock_command_runner.ts
@@ -1,14 +1,15 @@
 import { CommandResult, CommandRunner } from "../command_runner.ts";
 
+const defaultCommandResult: CommandResult = {
+  stdout: new Uint8Array(),
+  stderr: new Uint8Array(),
+  success: false,
+  code: 1,
+};
 
 export class MockCommandRunner implements CommandRunner {
-  public mockResult: CommandResult;
+  public mockResult: CommandResult = defaultCommandResult;
 
-  /**
-   * Mocks the runCommand method to return the predefined mockResult.
-   * @param args The command arguments (ignored in the mock).
-   * @returns A Promise resolving to the predefined CommandResult.
-   */
   async runCommand(args: string[]): Promise<CommandResult> {
     console.log(`MockCommandRunner: runCommand called with args: ${args}`);
     return Promise.resolve(this.mockResult);
